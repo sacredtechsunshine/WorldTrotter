f05faa9fb8e42f19edeb2cb503a5132eb7c75d50
April Stocker <astocke@jacksonville.edu>
2025-09-04 15:39:48 -0400

    Initial Commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by April on 9/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       }
+}
+
---
+++ WorldTrotterTests/WorldTrotterTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorldTrotterTests.swift
+//  WorldTrotterTests
+//
+//  Created by April on 9/4/25.
+//
+
+import Testing
+@testable import WorldTrotter
+
+struct WorldTrotterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
