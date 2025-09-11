59e8c380cc3243ed96febd9eff45e006abb86ac1
April Stocker <astocke@jacksonville.edu>
2025-09-11 16:30:58 -0400

    Finish commit

---
+++ WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by April on 9/4/25.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        print("ConversionViewController loaded its view.")
+        
+    }
+}
+
---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by April on 9/11/25.
+//
+
+import UIKit
+
+class MapViewController:UIViewController
+{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("MapViewController loaded its view.")
+    }
+}
+
